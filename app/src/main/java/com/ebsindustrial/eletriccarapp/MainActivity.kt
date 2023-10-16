374db6285be2481f93ba8b2cfe48dfe6bf7d1b6b
Emerson Barros Silva <ebsindustrial@gmail.com>
2023-10-16 16:47:59 -0300

    transicao entre tela

--- app/src/main/java/com/ebsindustrial/eletriccarapp/MainActivity.kt
+++ app/src/main/java/com/ebsindustrial/eletriccarapp/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.ebsindustrial.eletriccarapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var imgCar: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        imgCar = findViewById(R.id.img_car)
+
+        imgCar.setOnClickListener { detalhes() }
+
+    }
+
+    fun detalhes(){
+        startActivity(Intent(this, CalcularAutonomiaActivity::class.java))
     }
 }
